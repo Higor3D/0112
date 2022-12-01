6a1a76d329dcf5a24dfc14ae321d8d82b3c1d640
Higor3D <higorgomes07@gmail.com>
2022-12-01 20:16:27 -0300

    rr

--- app/src/main/java/br/com/etecia/a0112/MainActivity.java
+++ app/src/main/java/br/com/etecia/a0112/MainActivity.java
@@ -3,12 +3,47 @@ package br.com.etecia.a0112;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView ListaFilmes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListaFilmes = findViewById(R.id.Listafilmes);
+
+    Adaptador adapter = new Adaptador();
+
+        ListaFilmes.setAdapter(Adaptador);
     }
+
+    public class Adaptador extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
+    }
+
 }
